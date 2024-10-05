69d7fc0967dad88cde0081947c55e80509999822
vinicius-hayden <vinihaydenss@gmail.com>
2024-10-04 23:27:39 -0400

    adds iterative mapping on product list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
-import Image from "next/image";
 import ProductItem from "@/components/product/ProductItem";
 import { products } from "@/core";
 
 export default function Home() {
   return (
-    <div className="flex gap-5 flex-wrap"> 
-      <ProductItem product={products[0]}></ProductItem>
-      <ProductItem product={products[1]}></ProductItem>
-      <ProductItem product={products[2]}></ProductItem>
-      <ProductItem product={products[3]}></ProductItem>
+    <div className="grid grid-cols-4 gap-5 container">
+      { products.map((product) => (
+        <ProductItem key={product.id} product={product}/>
+      )) }
     </div>
   );
 }
